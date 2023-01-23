b683a2ece47b28810e44ca2e44871c60e7806948
tuniet <tofeju@gmail.com>
2023-01-23 17:15:02 +0000

    final

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, Character, FavouritePlanets, FavouriteCharacters
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,92 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route("/planets", methods=["GET"])
+def getPlanet():
+    planets = Planet.query.all()
+    response = [planet.serialize() for planet in planets]
+    response_body = {"message": "ok",
+                     "results": response
+    }
+    return jsonify(response_body), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
+@app.route("/planets/<int:planetid>", methods=["GET"])
+def getPlanetid(planetid):
+    planet = db.get_or_404(Planet, planetid)
+    response = planet.serialize()
+    response_body = {"message": "ok",
+                     "results": response
+    }
+    return jsonify(response_body), 200
+
+@app.route("/characters", methods=["GET"])
+def getCharacter():
+    characters = Character.query.all()
+    response = [character.serialize() for character in characters]
+    response_body = {"message": "ok",
+                     "results": response
+    }
+    return jsonify(response_body), 200
+
+@app.route("/characters/<int:characterid>", methods=["GET"])
+def getCharacterid(characterid):
+    character = db.get_or_404(Character, characterid)
+    response = character.serialize()
+    response_body = {"message": "ok",
+                     "results": response
+    }
+    return jsonify(response_body), 200
+
+@app.route('/users', methods=['GET'])
+def getUsers():
+    users = User.query.all()
+    response = [user.serialize() for user in users]
+    response_body = {"message": "ok",
+                     "results": response
     }
 
     return jsonify(response_body), 200
+
+@app.route('/user/<int:userid>/favourites', methods=['GET'])
+def getFavourites(userid):
+    characters = FavouriteCharacters.query.filter(FavouriteCharacters.userid == userid).all()
+    planets = FavouritePlanets.query.filter(FavouritePlanets.userid == userid).all()
+    planetResult = [planet.serialize() for planet in planets]
+    charResult = [character.serialize() for character in characters]
+    response_body = {"message": "ok",
+                     "planets": planetResult,
+                     "characters": charResult
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/<int:userId>/favourites/planet/<int:planetId>', methods=['POST'])
+def postFavouritePlanet(userId, planetId):
+    favPlanet = FavouritePlanets(userid = userId, planetid = planetId)
+    db.session.add(favPlanet)
+    db.session.commit()
+    return jsonify("Ok"), 200
+
+@app.route('/user/<int:userId>/favourites/character/<int:charId>', methods=['POST'])
+def postFavouriteCharacter(userId, charId):
+    favChar = FavouriteCharacters(userid = userId, characterid = charId)
+    db.session.add(favChar)
+    db.session.commit()
+    return jsonify("Ok"), 200
+
+#No se como hacer funcionar los DELETE :(
+@app.route('/user/<int:userId>/favourites/planet/<int:planetId>', methods=['DELETE'])
+def deleteFavouritePlanet(userId, planetId):
+    delete = FavouritePlanets.query.filter((FavouritePlanets.userid == userId) and (FavouritePlanets.planetid == planetId))
+    db.session.delete(delete)
+    db.session.commit()
+    return jsonify("Ok"), 200
+
+@app.route('/user/<int:userId>/favourites/character/<int:charId>', methods=['DELETE'])
+def deleteFavouriteCharacter(userId, charId):
+    delete = FavouriteCharacters.query.filter((FavouriteCharacters.userid == userId) and (FavouriteCharacters.characterid == charId))
+    db.session.delete(delete)
+    db.session.commit()
+    return jsonify("Ok"), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -3,10 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    is_active = db.Column(db.Boolean(), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +16,76 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    desc = db.Column(db.String(120), nullable = False)
+    hair_color = db.Column(db.String(120), nullable=False)
+    eye_color = db.Column(db.String(120), nullable=False)
+
+
+    def __repr__(self):
+        return '<Character %r>' % self.id
+
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name,
+                "desc": self.desc,
+                "hair_color": self.hair_color,
+                "eye_color": self.eye_color
+        }
+
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    desc = db.Column(db.String(120), nullable = False)
+    climate = db.Column(db.String(120), nullable=False)
+    population = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.id
+
+    def serialize(self):
+        return {"id": self.id,
+                "name": self.name,
+                "desc": self.desc,
+                "climate": self.climate,
+                "population": self.population
+        }
+
+class FavouritePlanets(db.Model):
+    __tablename__ = 'favouritePlanets'
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable = False)
+    planetid = db.Column(db.Integer, db.ForeignKey("planet.id"), nullable = False)
+
+    def __repr__(self):
+        return '<FavouritePlanets %r>' % self.id
+
+    def serialize(self):
+        return {"id": self.id,
+                "userid": self.userid,
+                "planetid": self.planetid
+        }
+
+class FavouriteCharacters(db.Model):
+    __tablename__ = 'favouriteCharacters'
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    characterid = db.Column(db.Integer, db.ForeignKey("character.id"), nullable=False)
+
+    def __repr__(self):
+        return '<FavouriteCharacters %r>' % self.id
+
+    def serialize(self):
+        return {"id": self.id,
+                "userid": self.userid,
+                "characterid": self.characterid
         }
